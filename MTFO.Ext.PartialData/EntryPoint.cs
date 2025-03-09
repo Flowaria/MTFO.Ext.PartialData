c307fdf21720849cb584e3a1de3711ef6774b3a1
Flowaria <wnsghgj1234@naver.com>
2025-03-09 10:51:33 +0900

    Bumped Again because of AWOFixer Version

--- MTFO.Ext.PartialData/EntryPoint.cs
+++ MTFO.Ext.PartialData/EntryPoint.cs
@@ -11,7 +11,7 @@ using System.IO;
 
 namespace MTFO.Ext.PartialData
 {
-    [BepInPlugin("MTFO.Extension.PartialBlocks", "MTFO pDataBlock", "1.5.0")]
+    [BepInPlugin("MTFO.Extension.PartialBlocks", "MTFO pDataBlock", "1.5.1")]
     [BepInProcess("GTFO.exe")]
     [BepInDependency(MTFOUtil.MTFOGUID, BepInDependency.DependencyFlags.HardDependency)]
     [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
